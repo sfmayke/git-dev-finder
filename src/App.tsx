ed4afca3480bb354afd058b379363d4f862d5736
Mayke F. Santos <sfmayke@gmail.com>
2023-02-13 22:14:12 -0300

    tailwind config and css debuging

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Hello world!</div>;
+  return (
+    <div className="App">
+      <div className="">
+        asdas <p>asdas</p>
+      </div>
+      <div>asdas</div>
+      <div>
+        <div>
+          <p>asdas</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,8 +6,6 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
-      mobile: '375px',
-      tablet: '768px',
       desktop: '1440px',
     },
     extend: {
